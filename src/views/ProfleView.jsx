a6943b0a10748939ba8a114ce411717d33d762cc
Carlos Estrada <carlos.deg02@hotmail.com>
2020-04-16 18:03:24 -0500

    Fixed missing import

--- src/views/ProfleView.jsx
+++ src/views/ProfleView.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image'
